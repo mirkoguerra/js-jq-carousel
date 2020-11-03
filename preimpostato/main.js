ba95f1724f5cc1ccadd60c293ca81b9583281dbd
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-03 16:50:13 +0100

    Cambio immagini e cambio pallino

--- preimpostato/main.js
+++ preimpostato/main.js
@@ -2,21 +2,28 @@
 $(document).ready(function(){
 
   // definizione variabili
-  var imgPrima = $(".first");
-  var imgUltima = $(".last");
+  var imgPrima = $("img.first");
+  var imgUltima = $("img.last");
+  var pallinoPrimo = $("i.first");
+  var pallinoUltimo = $("i.last");
 
   // gestisco il click della freccia sinistra
   $(".prev").click(function(){
 
     // definizione variabile attiva
     var imgAttiva = $(".slider-wrapper .images img.active");
+    var pallinoAttivo = $(".nav > i.active");
 
     imgAttiva.removeClass("active"); // faccio in modo che al click della freccia rivolta verso sinistra, la classe active, inizialmente presente solo sulla prima immagine, venga tolta, così da rendere il display none
+    pallinoAttivo.removeClass("active"); // stessa cosa per il pallino
+
     if (imgAttiva.hasClass("first")) {
       imgUltima.addClass("active");
+      pallinoUltimo.addClass("active");
     } else {
       imgAttiva.prev().addClass("active");
-    } // se sono sulla prima immagine passo all'ultima, altrimenti passo all'immagine precedente rispetto a quella visualizzata
+      pallinoAttivo.prev().addClass("active");
+    } // se sono sulla prima immagine passo all'ultima, altrimenti passo all'immagine precedente rispetto a quella visualizzata, stessa logica per i pallini
   });
 
   // gestisco il click della freccia destra
@@ -24,12 +31,17 @@ $(document).ready(function(){
 
     // definizione variabile attiva
     var imgAttiva = $(".slider-wrapper .images img.active");
+    var pallinoAttivo = $(".nav > i.active");
 
     imgAttiva.removeClass("active"); // faccio in modo che al click della freccia rivolta verso destra, la classe active, inizialmente presente solo sulla prima immagine, venga tolta, così da rendere il display none
+    pallinoAttivo.removeClass("active"); // stessa cosa per il pallino
+
     if (imgAttiva.hasClass("last")) {
       imgPrima.addClass("active");
+      pallinoPrimo.addClass("active");
     } else {
       imgAttiva.next().addClass("active");
+      pallinoAttivo.next().addClass("active");
     } // se sono sull'ultima immagine passo alla prima, altrimenti passo all'immagine successiva rispetto a quella visualizzata
   });
 
